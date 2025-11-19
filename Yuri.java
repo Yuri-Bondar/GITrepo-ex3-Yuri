778be3c495d580751817b8c9082c381e62d8559a
Yuri-Bondar <yuribon@gmail.com>
2025-11-19 16:00:41 +0200

    changed the print line 2.0

--- Yuri.java
+++ Yuri.java
@@ -1,7 +1,7 @@
 public class Yuri {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Yuri Bondar");
+            System.out.println("Sudo make me pass");
         }
     }
 }
